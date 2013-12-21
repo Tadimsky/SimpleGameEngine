fe2bbb156bc7740b37fd9dfe89127747b9405ffb
Jonno Schmidt <jonno.schmidt@duke.edu>
2013-12-20 22:24:58 -0700

    Fixed up constructor errors to make initialization of the engine work

--- src/base/SGEngine.cpp
+++ src/base/SGEngine.cpp
@@ -6,16 +6,51 @@
  */
 
 #include "SGEngine.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
 namespace sge_base {
 
-SGEngine::SGEngine() {
-	// TODO Auto-generated constructor stub
+SGEngine::SGEngine(const string& wndtext, int width, int height) {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+			SGEngine::logSDLError(cout, "SDL_Init");
+			return;
+	}
+	this->window = SDL_CreateWindow(wndtext.c_str(), 100, 100, width, height, SDL_WINDOW_SHOWN);
+		if (this->window == nullptr) {
+			logSDLError(cout, "CreateWindow");
+		return;
+	}
 
+	this->renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (renderer == nullptr) {
+		logSDLError(cout, "CreateRenderer");
+		return;
+	}
+
+	if ((IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG) != IMG_INIT_PNG) {
+		logSDLError(cout, "IMG_Init");
+		return;
+	}
 }
 
 SGEngine::~SGEngine() {
-	// TODO Auto-generated destructor stub
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	IMG_Quit();
+}
+
+void SGEngine::clearScreen() {
+	SDL_RenderClear(this->renderer);
+}
+
+void SGEngine::renderScreen() {
+	SDL_RenderPresent(this->renderer);
+}
+
+void SGEngine::logSDLError(ostream &os, const string &msg) {
+	os << msg << " : " << SDL_GetError() << endl;
 }
 
 } /* namespace sge_base */
--- src/base/SGEngine.h
+++ src/base/SGEngine.h
@@ -9,13 +9,32 @@
 #define SGENGINE_H_
 
 
+#include <SDL2/SDL.h>
+#include <iostream>
+#include <SDL2/SDL_image.h>
+
 using namespace std;
 namespace sge_base {
 
 class SGEngine {
+private:
+
+	SDL_Window * window;
+
 public:
-	SGEngine();
+	SGEngine(const string &wndtext, int width = 640, int height = 480);
+
+	SDL_Renderer * renderer;
+
 	virtual ~SGEngine();
+
+	SDL_Renderer * getRenderer();
+
+	void clearScreen();
+
+	void renderScreen();
+
+	static void logSDLError(ostream &os, const string &msg);
 };
 
 } /* namespace sge_base */
